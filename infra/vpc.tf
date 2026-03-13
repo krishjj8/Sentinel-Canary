bc7f8019bb831157995b651d402c33f3355899f5
krishjj8 <krishkumarj81@gmail.com>
2026-03-13 21:12:11 +0530

    fix: resolve nested git issue in infra folder

---
+++ infra/vpc.tf
@@ -0,0 +1,32 @@
+
+#VPC
+resource "aws_vpc" "sentinel_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {Name="sentinel_vpc"}
+}
+
+#IGW
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.sentinel_vpc.id
+}
+
+#Public Subnet
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.sentinel_vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.sentinel_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
