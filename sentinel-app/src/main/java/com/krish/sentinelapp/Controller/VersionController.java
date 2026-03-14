f116d28aedf1824895cf86731f220fbc47038e00
krishjj8 <krishkumarj81@gmail.com>
2026-03-14 23:33:38 +0530

    feat:containerized app with multi-stage build pushed to ECR

---
+++ sentinel-app/src/main/java/com/krish/sentinelapp/Controller/VersionController.java
@@ -0,0 +1,20 @@
+package  com.krish.sentinelapp.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class VersionController {
+
+    @GetMapping("/version")
+    public Map<String, String> version() {
+        return Map.of(
+                "version","v1",
+                "status","healthy"
+
+        );
+    }
+}
